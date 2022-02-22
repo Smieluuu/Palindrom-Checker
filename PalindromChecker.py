7bd942931fbbe0d736d4998c4fcfd655099aca5e
Kacper Śmielak <96179878+Smieluuu@users.noreply.github.com>
2022-02-22 15:31:50 +0100

    Palindrom-Checker
    Add Palindrom-Checker

---
+++ PalindromChecker.py
@@ -0,0 +1,10 @@
+tekst = input("wpisz text aby sprawdzić czy jest palindromem: ")
+
+if tekst == tekst[::-1]:
+    print('tekst jest palindromem')
+else:
+    print('tekst nie jest palindromem')
+
+
+
+
